a193c45bb4988f0dc4806d5040588b24ff95759c
MaGeiber <26193554+MaGeiber@users.noreply.github.com>
2023-03-19 21:57:11 -0700

    fix some bad behavior, change timer to use SIGALRM

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <pthread.h>
 #include <signal.h>
@@ -11,7 +12,7 @@
 #include <sys/queue.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <time.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 #define SOCK_PORT "9000"
@@ -20,6 +21,7 @@
 
 static bool graceful_exit = false;
 static pthread_mutex_t file_mutex;
+FILE * fp;
 
 struct socket_thread_args
 {
@@ -28,24 +30,20 @@ struct socket_thread_args
     int connected_handle;
     FILE * file_pointer;
     int done_flag;
+    pthread_mutex_t * file_mutex;
     SLIST_ENTRY(socket_thread_args) thread_entries;
-};
-
-struct timer_thread_args
-{
-    FILE * file_pointer;
 };
 
 static void signal_handler(int signal_number);
 static void cleanup(FILE * file_pointer, int socket_handle, int connected_handle, int exit_status);
 static void * socket_thread(void * args);
-static void * timer_thread(void * args);
+static void timer_signal();
 
 SLIST_HEAD(socket_list_head, socket_thread_args);
 
 int main(int argc, char *argv[])
 {
-    FILE * fp = NULL;
+    fp = NULL;
     int socket_handle;
     int bind_return;
     int listen_return;
@@ -58,8 +56,6 @@ int main(int argc, char *argv[])
     struct sigaction sig_action;
     char client_address[INET_ADDRSTRLEN];
     pid_t proc_id;
-    pthread_t timer_thread_id;
-    struct timer_thread_args * timer_args;
 
     struct socket_list_head head;
     struct socket_thread_args * temp_entry;
@@ -80,7 +76,7 @@ int main(int argc, char *argv[])
     {
         syslog(LOG_ERR, "Failed to register SIGINT signal!");
     }
-    
+
     // end socket loop after sigint or sigterm handler
     while(!graceful_exit)
     {
@@ -103,6 +99,8 @@ int main(int argc, char *argv[])
             cleanup(fp, socket_handle, connected_handle, -1);
         }
 
+
+
         int get_addr_return = getaddrinfo(NULL, SOCK_PORT, &hint, &returned_addr_info);
         if(get_addr_return != 0)
         {
@@ -115,7 +113,7 @@ int main(int argc, char *argv[])
         if(socket_handle == -1)
         {
             // failed to create handle, return
-            fprintf(stdout, "FAILED TO GET SOCKET HANDLE!\n");
+            //fprintf(stdout, "FAILED TO GET SOCKET HANDLE!\n");
             cleanup(fp, socket_handle, connected_handle, -1);
         }
 
@@ -127,7 +125,7 @@ int main(int argc, char *argv[])
         if(bind_return != 0)
         {
             // failed to bind, return
-            fprintf(stdout, "FAILED TO BIND! %d error: %d\n", bind_return, errno);
+            //fprintf(stdout, "FAILED TO BIND! %d error: %d\n", bind_return, errno);
             //cleanup(fp, socket_handle, connected_handle, -1);
         }
 
@@ -135,6 +133,7 @@ int main(int argc, char *argv[])
         {
             if(strcmp(argv[1], "-d") == 0)
             {
+                //fprintf(stdout, "Daemon Mode!!!\n");
                 proc_id = fork();
                 if(proc_id < 0)
                 {
@@ -146,8 +145,23 @@ int main(int argc, char *argv[])
                     // parent, exit
                     exit(0);
                 }
+                int status;
+                status = setsid();
+                status = chdir("/");
+                open("/dev/null", O_RDWR);
+                dup(0);
+                dup(0);
             }
         }
+
+        // register alarm signal handler
+        struct itimerval timestamp_itimer;
+        timestamp_itimer.it_value.tv_sec = 10;
+        timestamp_itimer.it_value.tv_usec = 0;
+        timestamp_itimer.it_interval.tv_sec = 10;
+        timestamp_itimer.it_interval.tv_usec = 0;
+        setitimer(ITIMER_REAL, &timestamp_itimer, NULL);
+        signal(SIGALRM, timer_signal);
 
         // free the malloc'd memory
         freeaddrinfo(returned_addr_info);
@@ -157,15 +171,9 @@ int main(int argc, char *argv[])
         if(listen_return != 0)
         {
             // failure from listen, return
-            fprintf(stdout, "FAILED TO LISTEN!\n");
+            //fprintf(stdout, "FAILED TO LISTEN!\n");
             cleanup(fp, socket_handle, connected_handle, -1);
         }
-
-        // start timestamp thread
-        timer_args = malloc(sizeof(struct timer_thread_args));
-        timer_args->file_pointer = fp;
-        fprintf(stdout, "Creating timer thread!\n");
-        pthread_create(&timer_thread_id, NULL, timer_thread, timer_args);
 
         while(!graceful_exit)
         {
@@ -173,44 +181,53 @@ int main(int argc, char *argv[])
             connected_handle = accept(socket_handle, &connected_addr, &sockaddr_size);
             if(connected_handle == -1)
             {
-                if(graceful_exit)
+                if(!graceful_exit)
                 {
-                    cleanup(fp, socket_handle, connected_handle, 0);
+                    // failure occured when accepting, return
+                    //fprintf(stdout, "FAILED TO ACCEPT!\n");
+                    cleanup(fp, socket_handle, connected_handle, -1);
                 }
-                // failure occured when accepting, return
-                fprintf(stdout, "FAILED TO ACCEPT!\n");
-                cleanup(fp, socket_handle, connected_handle, -1);
             }
-
-            // log message to syslog “Accepted connection from xxx” where XXXX is the IP address of the connected client
-            struct sockaddr_in *address = (struct sockaddr_in *)&connected_addr;
-            inet_ntop(AF_INET, &(address)->sin_addr, client_address, INET_ADDRSTRLEN);
-            syslog(LOG_INFO, "Accepted connection from %s", client_address);
-
-            // create thread
-            struct socket_thread_args * thread_args;
-            thread_args = malloc(sizeof(struct socket_thread_args));
-            thread_args->done_flag = false;
-            thread_args->connected_handle = connected_handle;
-            thread_args->file_pointer = fp;
-            thread_args->socket_handle = socket_handle;
-            fprintf(stdout, "Creating socket thread!\n");
-            pthread_create(&thread_args->thread_id, NULL, socket_thread, thread_args);
-
-            if(thread_args->thread_id != 0)
+            else
             {
-                fprintf(stdout, "Thread created, adding to list!\n");
-                // add thread to list
-                SLIST_INSERT_HEAD(&head, thread_args, thread_entries);
+                // log message to syslog “Accepted connection from xxx” where XXXX is the IP address of the connected client
+                struct sockaddr_in *address = (struct sockaddr_in *)&connected_addr;
+                inet_ntop(AF_INET, &(address)->sin_addr, client_address, INET_ADDRSTRLEN);
+                syslog(LOG_INFO, "Accepted connection from %s", client_address);
+
+                // create thread
+                struct socket_thread_args * thread_args;
+                thread_args = malloc(sizeof(struct socket_thread_args));
+                memset(thread_args, 0, sizeof(thread_args));
+                thread_args->done_flag = false;
+                thread_args->connected_handle = connected_handle;
+                thread_args->file_pointer = fp;
+                thread_args->socket_handle = socket_handle;
+                thread_args->file_mutex = &file_mutex;
+                //fprintf(stdout, "Main: Creating socket thread, connected handle: %d!\n", connected_handle);
+                int create_result = pthread_create(&thread_args->thread_id, NULL, socket_thread, thread_args);
+
+                if(create_result == 0)
+                {
+                    //fprintf(stdout, "Main: Thread created: %lu, adding to list!\n", thread_args->thread_id);
+                    // add thread to list
+                    SLIST_INSERT_HEAD(&head, thread_args, thread_entries);
+                }
+                else
+                {
+                    //fprintf(stdout, "Main: Failure to create thread!\n");
+                }
             }
 
             // check threads, join if done
             SLIST_FOREACH(temp_entry, &head, thread_entries)
                 if(temp_entry->done_flag == true && temp_entry->thread_id != 0)
                 {
-                    fprintf(stdout, "Thread completed, joining!\n");
+                    //fprintf(stdout, "Main: Thread completed: %lu, joining!\n", temp_entry->thread_id);
                     pthread_join(temp_entry->thread_id, NULL);
                     temp_entry->thread_id = 0;
+                    shutdown(temp_entry->connected_handle, SHUT_RDWR);
+                    close(temp_entry->connected_handle);
                 }
 
         }
@@ -220,7 +237,7 @@ int main(int argc, char *argv[])
     syslog(LOG_INFO, "Closed connection from %s", client_address);
 
     // wait for all threads to complete
-    fprintf(stdout, "Closing server, waiting for threads to complete!\n");
+    //fprintf(stdout, "Closing server, waiting for threads to complete!\n");
     bool waiting = true;
     while(waiting)
     {
@@ -231,8 +248,11 @@ int main(int argc, char *argv[])
             {
                 if(temp_entry->thread_id != 0)
                 {
+                    //fprintf(stdout, "Main: Thread completed during close: %lu, joining!\n", temp_entry->thread_id);
                     pthread_join(temp_entry->thread_id, NULL);
                     temp_entry->thread_id = 0;
+                    shutdown(temp_entry->connected_handle, SHUT_RDWR);
+                    close(temp_entry->connected_handle);
                 }
             }
             else
@@ -240,10 +260,6 @@ int main(int argc, char *argv[])
                 waiting = true;
             }
     }
-
-    // join timer thread
-    pthread_join(timer_thread_id, NULL);
-    free(timer_args);
 
     // clean up memory
     while(!SLIST_EMPTY(&head))
@@ -253,7 +269,7 @@ int main(int argc, char *argv[])
         free(temp_entry);
     }
 
-    fprintf(stdout, "All threads complete, cleaning up!\n");
+    //fprintf(stdout, "All threads complete, cleaning up!\n");
     cleanup(fp, socket_handle, connected_handle, 0);
 }
 
@@ -281,6 +297,7 @@ static void cleanup(FILE * file_pointer, int socket_handle, int connected_handle
 {
     if(socket_handle > 0)
     {
+        shutdown(socket_handle, SHUT_RDWR);
         close(socket_handle);
     }
 
@@ -307,11 +324,7 @@ static void * socket_thread(void * args)
     struct socket_thread_args *thread_args;
     thread_args = (struct socket_thread_args *)args;
 
-    fprintf(stdout, "Thread created, trying to grab mutex!\n");
-
-    while(pthread_mutex_trylock(&file_mutex) != 0);
-
-    fprintf(stdout, "Grabbed mutex!\n");
+    //fprintf(stdout, "Socket Thread: Thread started: %lu!\n", thread_args->thread_id);
 
     // receive data from connection and append to file
     buffer_bytes_used = 0;
@@ -326,12 +339,14 @@ static void * socket_thread(void * args)
         bytes_received = recv(thread_args->connected_handle, &buffer[buffer_bytes_used], buffer_bytes_remaining, 0);
         if(bytes_received > 0)
         {
-            fprintf(stdout, "Received %ld bytes\n", bytes_received);
+            //fprintf(stdout, "Received %ld bytes\n", bytes_received);
             buffer_bytes_used += bytes_received;
         }
         else if(bytes_received < 0)
         {
-            fprintf(stdout, "Received error: %ld, errno: %d\n", bytes_received, errno);
+            //fprintf(stdout, "Received error: %ld, errno: %d\n", bytes_received, errno);
+            thread_args->done_flag = true;
+            pthread_exit(0);
         }
         else
         {
@@ -343,32 +358,41 @@ static void * socket_thread(void * args)
         found_newline = (strchr(buffer, '\n') != NULL) ? true : false;
         if(found_newline || buffer_bytes_used >= BUFFER_SIZE)
         {
-            fprintf(stdout, "Found newline: %d\n", found_newline);
+            //fprintf(stdout, "Found newline: %d\n", found_newline);
             int store_return;
+            
+            pthread_mutex_lock(thread_args->file_mutex);
+            //fprintf(stdout, "Socket Thread: Grabbed mutex, writing data!\n");
             store_return = fwrite(buffer, sizeof(char), buffer_bytes_used, thread_args->file_pointer);
+            pthread_mutex_unlock(thread_args->file_mutex);
+            //fprintf(stdout, "Socket Thread: Mutex unlocked!\n");
+
             if(store_return == EOF)
             {
                 syslog(LOG_ERR, "Failed to append to file!");
-                //cleanup(thread_args->file_pointer, thread_args->socket_handle, thread_args->connected_handle, -1);
-                pthread_exit(-1);
+                thread_args->done_flag = true;
+                pthread_exit(0);
             }
-            fprintf(stdout, "Stored message\n");
+            //fprintf(stdout, "Stored message\n");
             buffer_bytes_used = 0;
             memset(&buffer, 0, sizeof(buffer));
         }
     } while (bytes_received > 0 && !found_newline);
-    fprintf(stdout, "Done receiving\n");
+    //fprintf(stdout, "Done receiving\n");
     
-
+    pthread_mutex_lock(thread_args->file_mutex);
+    //fprintf(stdout, "Socket Thread: Grabbed mutex, reading back!\n");
     // send content of file back to sender
     int seek_return = fseek(thread_args->file_pointer, 0, SEEK_SET);
     if(seek_return < 0)
     {
-        fprintf(stdout, "Failed to seek while reading: %d\n", errno);
+        //fprintf(stdout, "Failed to seek while reading: %d\n", errno);
     }
     ssize_t bytes_sent = 0;
     memset(&buffer, 0, sizeof(buffer));
     size_t bytes_read = fread(buffer, sizeof(char), BUFFER_SIZE, thread_args->file_pointer);
+    
+    //fprintf(stdout, "Socket Thread: Unlocked mutex!\n");
     while(bytes_read > 0)
     {
         size_t read_until = bytes_read;
@@ -378,7 +402,7 @@ static void * socket_thread(void * args)
         ssize_t send_return = 0;
         while(bytes_sent < read_until)
         {
-            fprintf(stdout, "Sending bytes until: %lu\n", read_until);
+            //fprintf(stdout, "Sending bytes until: %lu\n", read_until);
             send_return = send(thread_args->connected_handle, &buffer[bytes_sent], read_until, 0);
             
             if(send_return > 0)
@@ -387,59 +411,50 @@ static void * socket_thread(void * args)
             }
             else
             {
-                fprintf(stdout, "Send return val: %ld\n", send_return);
+                //fprintf(stdout, "Send Failure, return val: %ld\n", send_return);
+                thread_args->done_flag = true;
+                pthread_exit(0);
             }
         }
         bytes_read = fread(buffer, sizeof(char), BUFFER_SIZE, thread_args->file_pointer);
         bytes_sent = 0;
     }
-    fprintf(stdout, "Done sending\n");
-    pthread_mutex_unlock(&file_mutex);
+    pthread_mutex_unlock(thread_args->file_mutex);
+    //fprintf(stdout, "Done sending\n");
 
     thread_args->done_flag = true;
     pthread_exit(0);
 }
 
-static void * timer_thread(void * args)
+static void timer_signal()
 {
-    struct timer_thread_args *timer_args;
-    timer_args = (struct timer_thread_args *)args;
-    clock_t start_time;
     clock_t current_time;
     time_t time_to_output;
     struct tm *tmp;
     char output_string[200];
+    size_t string_length;
     memset(output_string, 0, sizeof(output_string));
 
-    fprintf(stdout, "Timer Thread: starting timer!\n");
-    start_time = clock();
+    //fprintf(stdout, "Timer Thread: starting timer!\n");
 
-    while(!graceful_exit)
+    current_time = clock();
+    //fprintf(stdout, "Timer Thread: outputting time!\n");
+    time_to_output = time(NULL);
+    tmp = localtime(&time_to_output);
+
+    string_length = strftime(output_string, sizeof(output_string), "timestamp:%F %H %M %S\n", tmp);
+    if(string_length != 0)
     {
-        current_time = clock();
-        if((current_time - start_time) > 10 * CLOCKS_PER_SEC)
+        //fprintf(stdout, "Timer Thread: attempting to grab mutex!\n");
+        pthread_mutex_lock(&file_mutex);
+        //fprintf(stdout, "Timer Thread: grabbed mutex, writing to file!\n");
+        int file_write_return;
+        file_write_return = fwrite(output_string, sizeof(char), string_length, fp);
+        if(file_write_return == EOF)
         {
-            fprintf(stdout, "Timer Thread: outputting time!\n");
-            time_to_output = time(NULL);
-            tmp = localtime(&time_to_output);
-
-            if(strftime(output_string, sizeof(output_string), "timestamp:%F %H %M %S\n", tmp) != 0)
-            {
-                fprintf(stdout, "Timer Thread: attempting to grab mutex!\n");
-                while(pthread_mutex_trylock(&file_mutex) != 0);
-                fprintf(stdout, "Timer Thread: grabbed mutex, writing to file!\n");
-                int file_write_return;
-                file_write_return = fwrite(output_string, sizeof(char), sizeof(output_string), timer_args->file_pointer);
-                if(file_write_return == EOF)
-                {
-                    syslog(LOG_ERR, "Failed to write timestamp!");
-                }
-                fprintf(stdout, "Timer Thread: unlocking mutex!\n");
-                pthread_mutex_unlock(&file_mutex);
-            }
-            start_time = clock();
+            syslog(LOG_ERR, "Failed to write timestamp!");
         }
+        //fprintf(stdout, "Timer Thread: unlocking mutex!\n");
+        pthread_mutex_unlock(&file_mutex);
     }
-
-    pthread_exit(0);
 }
