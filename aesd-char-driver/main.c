a79fb646eed9c7b33ef876c734f0245e27654efd
MaGeiber <26193554+MaGeiber@users.noreply.github.com>
2023-04-07 20:53:13 -0700

    open, release, and read implementation

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -16,31 +16,51 @@
 #include <linux/printk.h>
 #include <linux/types.h>
 #include <linux/cdev.h>
+#include <linux/mutex.h>
+#include <linux/slab.h>
 #include <linux/fs.h> // file_operations
 #include "aesdchar.h"
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
-MODULE_AUTHOR("Your Name Here"); /** TODO: fill in your name **/
+MODULE_AUTHOR("Matt Geib");
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
 
 int aesd_open(struct inode *inode, struct file *filp)
 {
+    struct aesd_dev *dev;
     PDEBUG("open");
-    /**
-     * TODO: handle open
-     */
+    
+    dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
+
+    aesd_circular_buffer_init(&dev->circ_buffer);
+    mutex_init(&dev->lock);
+
+    filp->private_data = dev;
     return 0;
 }
 
 int aesd_release(struct inode *inode, struct file *filp)
 {
+    struct aesd_dev *dev;
+    uint8_t index;
+    struct aesd_buffer_entry *entry;
+
     PDEBUG("release");
-    /**
-     * TODO: handle release
-     */
+
+    dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
+
+    // free memory used by circular buffer
+    AESD_CIRCULAR_BUFFER_FOREACH(entry,&dev->circ_buffer,index)
+    {
+        kfree(entry->buffptr);
+    }
+    
+    // destroy mutex
+    mutex_destroy(&dev->lock);
+    
     return 0;
 }
 
@@ -48,10 +68,64 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
                 loff_t *f_pos)
 {
     ssize_t retval = 0;
+    struct aesd_buffer_entry *read_entry;
+    int mutex_ret;
+    size_t entry_offset_byte;
+    unsigned long copy_user_return;
+    unsigned long bytes_in_entry;
+    unsigned long bytes_to_copy;
+    
+    struct aesd_dev *dev = filp->private_data;
+
     PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
-    /**
-     * TODO: handle read
-     */
+
+    // lock mutex before doing anything
+    mutex_ret = mutex_lock_interruptible(&dev->lock);
+    if(mutex_ret != 0)
+    {
+        // couldn't lock the mutex, something happened, return a fault
+        return -EFAULT;
+    }
+
+    // get the node from the buffer based on the given position
+    read_entry = aesd_circular_buffer_find_entry_offset_for_fpos(&dev->circ_buffer, *f_pos, &entry_offset_byte);
+
+    if(read_entry == NULL)
+    {
+        *f_pos = 0;
+        goto release_mutex;
+    }
+
+    // determine how much to copy to user buffer
+    bytes_in_entry = read_entry->size - entry_offset_byte;
+
+    // if more bytes were requested than are in the entry, read out the requested and update f_pos
+    if(bytes_in_entry > count)
+    {
+        bytes_to_copy = count;
+        *f_pos += count;
+    }
+    else
+    {
+        bytes_to_copy = bytes_in_entry;
+        *f_pos += bytes_in_entry;
+    }
+
+    // copy data to user space
+    copy_user_return = copy_to_user(buf, read_entry->buffptr + entry_offset_byte, bytes_to_copy);
+    if(copy_user_return)
+    {
+        retval = -EFAULT;
+        goto release_mutex;
+    }
+
+    // update return value to number of bytes read
+    retval = bytes_to_copy;
+
+    // release mutex, always
+    release_mutex:
+        mutex_unlock(&dev->lock);
+
     return retval;
 }
 
@@ -59,10 +133,36 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
                 loff_t *f_pos)
 {
     ssize_t retval = -ENOMEM;
+    int mutex_ret;
+
+    struct aesd_dev *dev = filp->private_data;
+
     PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
     /**
      * TODO: handle write
      */
+    // lock mutex
+    mutex_ret = mutex_lock_interruptible(&dev->lock);
+    if(mutex_ret != 0)
+    {
+        // couldn't lock the mutex, something happened, return a fault
+        return -EFAULT;
+    }
+
+    // copy data to kernel space
+    //copy_from_user()
+
+    // check for newline
+
+    // add entry to circular buffer
+    // aesd_circular_buffer_add_entry()
+
+    // free any allocated memory
+    free_mem:
+
+    // release mutex, always
+    release_mutex:
+        mutex_unlock(&dev->lock);
     return retval;
 }
 struct file_operations aesd_fops = {
